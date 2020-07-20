06b16f2ba7dfcbe1efc327604aa5efe815341526
x300n <houseofxeon@gmail.com>
2020-07-20 18:25:40 +0200

    API-docs were added using Swagger

--- src/main/java/org/ahmedgaber/Redditclone/RedditCloneApplication.java
+++ src/main/java/org/ahmedgaber/Redditclone/RedditCloneApplication.java
@@ -1,11 +1,14 @@
 package org.ahmedgaber.Redditclone;
 
+import org.ahmedgaber.Redditclone.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfig.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/ahmedgaber/Redditclone/config/SecurityConfig.java
+++ src/main/java/org/ahmedgaber/Redditclone/config/SecurityConfig.java
@@ -31,11 +31,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/subreddit")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/**")
+                .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
